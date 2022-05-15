d5113df9bd5ffbc29c2d25eaf13268c576ef6153
HandsomeDingor <jay807541931@gmail.com>
2022-05-15 02:44:28 -0400

    server add schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,49 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(saveBook: saveBookInput): User
+    removeBook(bookId: String!): User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    saveBooks: [saveBook]
+  }
+
+  type saveBook {
+    _id: ID
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input saveBookInput {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+  }
+
+`;
+
+module.exports = typeDefs;
