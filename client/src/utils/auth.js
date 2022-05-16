462458d14e737d8761f36f041c18e3b2eaa7f36d
HandsomeDingor <jay807541931@gmail.com>
2022-05-16 00:02:12 -0400

    fix

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -6,6 +6,7 @@ class AuthService {
   }
 
   loggedIn() {
+    // Checks if there is a saved token and it's still valid
     const token = this.getToken();
     return !!token && !this.isTokenExpired(token); // handwaiving here
   }
@@ -22,18 +23,24 @@ class AuthService {
   }
 
   getToken() {
+    // Retrieves the user token from localStorage
     return localStorage.getItem('id_token');
   }
 
   login(idToken) {
+    // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
+
     window.location.assign('/');
   }
 
   logout() {
+    // Clear user token and profile data from localStorage
+    // axios.defaults.headers.common["Authorization"] = null;
     localStorage.removeItem('id_token');
+    // this will reload the page and reset the state of the application
     window.location.assign('/');
   }
-};
+}
 
 export default new AuthService();
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,10 +6,11 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+        const userData = await User.findOne({ _id: context.user._id })
+        .select('-__v -password');
         return userData;
       }
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError('Not logged in');
     },
   },
 
@@ -23,7 +24,7 @@ const resolvers = {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError('No user found');
+        throw new AuthenticationError('Incorrect credentials');
       }
 
       const correctPw = await user.isCorrectPassword(password);
